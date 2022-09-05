ebf9e9c57328cd8991aa00f1182017a9243aac8f
vladimir <grun_gespenst@tut.by>
2022-09-05 18:27:41 +0300

    bug found

--- app/models.py
+++ app/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class City(models.Model):
-    name = models.CharField(max_length=30, verbose_name='City', null=True,
-                            blank=True)
+    name = models.CharField(max_length=30, verbose_name='City', null=True)
     members = models.ManyToManyField(User)
     def __str__(self):
         return self.name
