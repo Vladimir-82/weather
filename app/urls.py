8c275f3bc8534e1784253997edf011a79622943b
vladimir <grun_gespenst@tut.by>
2022-09-17 11:55:44 +0300

    button added

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('detail/', name='detail'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
